1bbf194662a494e2ae3f7319d429954548a4e85e
NITIN310806 <kishorenitin2006@gmail.com>
2026-03-16 08:58:24 +0530

    Add files via upload

---
+++ HotelBookingApp.java
@@ -0,0 +1,9 @@
+public class HotelBookingApp {
+    public static void main(String[] args) {
+
+        System.out.println("     Welcome to Book My Stay App    ");
+        System.out.println("Application Name : Hotel Booking Management System");
+        System.out.println("Version          : v1.0");
+        System.out.println("Application started successfully!");
+    }
+}
